6f8dec4d4ee7032e9efd14974606b09135772ea7
Gowthami0812 <gowthamivelumani1@gmail.com>
2025-11-16 12:18:10 +0530

    Add files via upload

--- Payment.java
+++ Payment.java
@@ -1,29 +1,3 @@
-interface Payment {
-    void pay();
+public interface Payment {
+    boolean pay(double amount);
 }
-
-class CreditCard implements Payment {
-    public void pay() {
-        System.out.println("Paid using Credit Card");
-    }
-}
-
-class PayPal implements Payment {
-    public void pay() {
-        System.out.println("Paid using PayPal");
-    }
-}
-
-class Cash implements Payment {
-    public void pay() {
-        System.out.println("Paid using Cash");
-    }
-}
-
-class Main {
-    public static void main(String[] args) {
-        Payment p = new PayPal();
-        p.pay();
-    }
-}
-
